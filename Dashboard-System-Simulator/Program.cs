620e056d056531eceb8bdb8877608273999d783b
Mahdi Kerdarian <mahdi.k.139515@gmail.com>
2025-07-31 10:45:18 +0330

    add Main Menu Method

--- Dashboard-System-Simulator/Program.cs
+++ Dashboard-System-Simulator/Program.cs
@@ -8,12 +8,20 @@ namespace Dashboard_System_Simulator
 {
     internal class Program
     {
+        static void Main_Menu()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Login / Register Menu:");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("1.Login \n2.Register \n3.Exit");
+            Console.ResetColor();
+        }
+
         static void Main(string[] args)
         {
             int Opration = 0;
             while (Opration != 3)
             {
-
             }
         }
     }
