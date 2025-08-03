9df86338764efb3244cf2458dda7ac54e0aae525
Mahdik-Dev <mahdi.kerdarian.dev@gmail.com>
2025-08-03 10:58:04 +0330

    Creat Menu Class And Methods

---
+++ Dashboard-System-Simulator/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashboard_System_Simulator
+{
+    public class Menu
+    {
+        public static void MainMenu()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("-----Main Menu-----");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("1.Login \n2.Register \n3.Exit");
+            Console.WriteLine("<<--------------->>");
+            Console.ResetColor();
+        }
+        public static void Profile()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("-----Profile Menu-----");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("1.Show Profile \n2.Change Password \n3.Delet Account \n4.Log Out \n5.Exit");
+            Console.WriteLine("<<--------------->>");
+            Console.ResetColor();
+        }
+    }
+}
