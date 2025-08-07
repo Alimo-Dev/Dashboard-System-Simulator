6c7e60df72a4b2d16a687b0fc9157a53c7c389d2
Mahdik-Dev <mahdi.kerdarian.dev@gmail.com>
2025-08-07 10:49:25 +0330

    Creat Classes of Opration and Tool Then Creat Methods Of Login and To Exist

---
+++ Dashboard-System-Simulator/Opration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashboard_System_Simulator
+{
+    internal class Opration
+    {
+        public static void Login()
+        {
+        }
+
+    }
+}
