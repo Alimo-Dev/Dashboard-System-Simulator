7ab02f2a202a1cdac6f8ee3eba757012ca83a942
Ali Mohammadi <alimohammadi.dev@gmail.com>
2025-08-07 09:50:46 +0330

    Create life cycle

---
+++ ConsoleApp1/ConsoleApp1/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Animal
+    {
+        public virtual void Run()
+        {
+            Console.WriteLine("Run");
+        }
+
+        public virtual void Sound()
+        {
+            Console.WriteLine("Sound");
+        }
+    }
+}
