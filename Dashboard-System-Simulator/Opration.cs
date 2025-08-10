7b523d7bf2abdbee8a464c35f8e8141434ebeeb3
Mahdik-Dev <mahdi.kerdarian.dev@gmail.com>
2025-08-10 10:38:47 +0330

    Creat Register Method

--- Dashboard-System-Simulator/Opration.cs
+++ Dashboard-System-Simulator/Opration.cs
@@ -50,5 +50,56 @@ namespace Dashboard_System_Simulator
                 Console.ResetColor();
             }
         }
+        //--------------------------------
+        // Register method
+        //--------------------------------
+        public static void Register(List<string> userNames, List<string> passWords, List<string> fullNames, List<string> emails, List<string> phoneNumbers, string user, string pass, string fName, string email, string phone, ref string currentUser)
+        {
+        Again:
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("-----Register Menu-----");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Username: ");
+            user = Console.ReadLine().ToLower().Trim();
+            Console.Write("Full Name: ");
+            fName = Console.ReadLine().ToLower().Trim();
+            Console.Write("Password: ");
+            pass = Console.ReadLine().Trim();
+            Console.Write("Email: ");
+            email = Console.ReadLine().ToLower().Trim();
+            Console.Write("Phone Number: ");
+            phone = Console.ReadLine().Trim();
+            Console.ResetColor();
+
+            if (Tool.ToExist(userNames, user) == false)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Tnx For Your Registertion");
+                Console.ResetColor();
+                userNames.Add(user);
+                passWords.Add(pass);
+                fullNames.Add(fName);
+                emails.Add(email);
+                phoneNumbers.Add(phone);
+                currentUser = user;
+            }
+            else
+            {
+                if (Tool.ToExist(emails, email) == true)
+                {
+                    Console.Clear();
+                    Console.ForegroundColor= ConsoleColor.DarkRed;
+                    Console.WriteLine("This Email Has Already Been Registered, Go To The Login Page");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                    Console.WriteLine("Your UserName Is Already To Exist");
+                    Console.ResetColor();
+                    goto Again;
+                }
+            }
+        }
     }
 }
